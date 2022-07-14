74fde52956803a08b8730411dc98dc973eb29236
lzphellipe <luizfelipealveslopes9@gmail.com>
2022-07-14 14:03:41 -0300

    git comiit  atualização aulas

--- src/OperadoresArimeticos.java
+++ src/OperadoresArimeticos.java
@@ -5,6 +5,20 @@ public class OperadoresArimeticos {
         int notaAluno2 = 80;
         int notaAluno3 = 53;
 
+      /*  int soma = 2+10;
+        int subtracao = 6-10;
+        int multiplicacao = 8*3;
+        int divisao = 8/2;
+        int resto = 7 %2;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+        System.out.println(resto);*/
+
+
+
         int totalGeral = notaAluno1 + notaAluno2 + notaAluno3;
         System.out.println("Total geral: " + totalGeral);
 
